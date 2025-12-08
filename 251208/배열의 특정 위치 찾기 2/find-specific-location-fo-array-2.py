b32c3002f76a71b65a1146fc6bd0f56d636ab3c2
b.upline_00 <sano820@naver.com>
2025-12-08 10:10:39 +0900

    Title: 배열의 특정 위치 찾기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 251208/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
+++ 251208/배열의 특정 위치 찾기 2/find-specific-location-fo-array-2.py
@@ -1,10 +1,11 @@
 nums = list(map(int, input().split()))
 
 a ,b = 0, 0
+# 홀수 합
 for i in range(0,10,2):
     a += nums[i]
-
+# 짝수 합
 for j in range(1,10,2):
     b += nums[j]
 
-print(b-a)
+print(max(a,b) - min(a,b))
